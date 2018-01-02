8861634cef3a90a537b4c00076f208ebeeaa375a
merfemor <efremov.roman98@gmail.com>
2018-01-02 20:34:47 +0300

    added trabslatable syntax

---
+++ src/Syntax/Translatable.hs
@@ -0,0 +1,39 @@
+module Syntax.Translatable where
+
+import Syntax.Abstract (UnaryOperation, BinaryOperation, Type)
+import qualified Data.Map as Map
+
+data Var = Var { varType :: Type, varName :: String } deriving Show
+
+type Id = Int
+
+data VariableId = VariableId { funcId :: Id
+                             , varId :: Id
+                             , isArgument :: Bool
+                             } deriving Show
+
+data Function = Function { returnType :: Maybe Type
+                         , funcName :: String
+                         , localVars :: [Var]
+                         , arguments :: [Var]
+                         , outerFunctionId :: Maybe Id
+                         , functionBody :: [Statement]
+                         } deriving Show
+                         
+data Expression = SLit Id
+                | ILit Int
+                | DLit Double
+                | UnaryExpression UnaryOperation Expression
+                | BinaryExpression BinaryOperation Expression Expression
+                | FCall Id [Expression]
+                | VCall VariableId
+                deriving Show
+                
+data Statement = VarAssign VariableId  Expression
+               | WhileLoop Expression [Statement]
+               | IfElse Expression [Statement] [Statement]
+               | Return (Maybe Expression)
+               | StFCall Id [Expression] deriving Show
+
+-- string pool and function list
+type TranslatableProgramTree = ([String], [Function])
---
+++ src/Translator.hs
@@ -0,0 +1,7 @@
+module Translator where
+
+import Syntax.Translatable as T
+import Syntax.Abstract     as A
+
+abstractToTranslatable :: AbstractProgramTree -> TranslatableProgramTree
+abstractToTranslatable = undefined
